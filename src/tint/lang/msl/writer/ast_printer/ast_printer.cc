938f28b8589ab56c71dd96bc56adb67c3809432d
Natalie Chouinard <chouinard@google.com>
2024-08-13 19:49:09 +0000

    WIP
    Change-Id: I159ef3bea2df1729c814400e6ecfe63afddb505b

--- src/tint/lang/msl/writer/ast_printer/ast_printer.cc
+++ src/tint/lang/msl/writer/ast_printer/ast_printer.cc
@@ -972,16 +972,15 @@ bool ASTPrinter::EmitBuiltinCall(StringStream& out,
         }
 
         case wgsl::BuiltinFn::kQuadBroadcast: {
-            // The lane argument is ushort.
             out << "quad_broadcast(";
             if (!EmitExpression(out, expr->args[0])) {
                 return false;
             }
-            out << ",ushort(";
+            out << ",";
             if (!EmitExpression(out, expr->args[1])) {
                 return false;
             }
-            out << "))";
+            out << ")";
             return true;
         }
 
