d08c0ca5a5b5b3061d6316eef241e6f64aac9360
Natalie Chouinard <chouinard@google.com>
2026-01-28 14:04:10 -0800

    Fix expectations

--- test/tint/bug/tint/1541.wgsl.expected.dxc.hlsl
+++ test/tint/bug/tint/1541.wgsl.expected.dxc.hlsl
@@ -2,6 +2,8 @@
 [numthreads(1, 1, 1)]
 void main() {
   bool a = true;
-  bool v = ((false) ? (true) : ((a & true)));
+  bool v_1 = (a & true);
+  bool v_2 = true;
+  bool v = ((false) ? (v_2) : (v_1));
 }
 
